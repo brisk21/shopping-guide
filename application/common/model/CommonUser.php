4c00af8782b08b159baa4efe5aa14b0840ccd8ed
琅玡-琪拉 <1076963452@qq.com>
2022-06-29 22:34:30 +0800

    新增会员管理

--- application/common/model/CommonUser.php
+++ application/common/model/CommonUser.php
@@ -4,10 +4,70 @@
 namespace app\common\model;
 
 
+use traits\model\SoftDelete;
+
 class CommonUser extends Base
 {
+    use SoftDelete;
+    protected $createTime = 'add_time';
+    protected $updateTime = 'up_time';
+    protected $deleteTime = 'del_time';
+    protected $autoWriteTimestamp = true;
+    protected $hidden = ['pwd','salt'];
+
+    protected $append = ['status_text'];
+
     public function getUidByUinodCode($unionCode)
     {
         return $this->where(['union_code'=>trim($unionCode)])->value('uid');
+    }
+
+    public function getStatusTextAttr($value, $data)
+    {
+        if (!isset($data['status'])) return '';
+        return config('param.member')['status'][$data['status']];
+    }
+
+    public function addData($data, $isSaveAll = false)
+    {
+        if ($isSaveAll) {
+            return self::allowField(true)->saveAll($data);
+        }
+        return self::allowField(true)->save($data);
+    }
+
+    public function saveData($id, $data, $isSaveAll = false)
+    {
+        if ($isSaveAll) {
+            return self::allowField(true)->isUpdate(true)->saveAll($data);
+        }
+        return self::allowField(true)->isUpdate(true)->save($data, ['id' => $id]);
+    }
+
+
+    public function fetchData($condition)
+    {
+        return self::get(function ($query) use ($condition) {
+            if (!empty($condition['uid'])) $query->where(['uid' => $condition['uid']]);
+            if (!empty($condition['id'])) $query->where(['id' => $condition['id']]);
+            if (!empty($condition['account'])) $query->where(['account' => trim($condition['account'])]);
+            if (!empty($condition['union_code'])) $query->where(['union_code' => intval($condition['union_code'])]);
+        });
+    }
+
+    public function listData($condition = [], $page = 1, $pageSize = 10, $order = 'id desc')
+    {
+        return $this
+            ->where(function ($query) use ($condition) {
+                if (!empty($condition['uid'])) $query->where(['uid' => $condition['uid']]);
+                if (isset($condition['status'])) $query->where(['status' => $condition['status']]);
+                if (!empty($condition['account'])) $query->where(['account' => trim($condition['account'])]);
+                if (!empty($condition['union_code'])) $query->where(['union_code' => intval($condition['union_code'])]);
+            })
+            ->field(true)
+            ->order($order)
+            ->page($page, $pageSize)
+            ->paginate($pageSize, false, ['page' => $page]);
+
     }
 }
---
+++ application/manager/controller/Member.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace app\manager\controller;
+
+
+use app\common\model\CommonUser;
+use think\Request;
+
+class Member extends Base
+{
+    protected $model;
+    public function __construct(Request $request = null)
+    {
+        parent::__construct($request);
+        $this->model = new CommonUser();
+    }
+
+    public function list_data()
+    {
+        $page = request()->post('page',1);
+        $pageSize = request()->post('pageSize',20);
+        $where = request()->only('union_code,account,nickname');
+
+        $data = $this->model->listData($where,$page,$pageSize);
+        data_return('success',0,$data);
+    }
+
+    public function save_data(Request $request)
+    {
+        if (!$request->isPut()){
+            data_return('非法操作',-1);
+        }
+        $param = $request->only('status,id,uid,nickname');
+        if (empty($param['id'])){
+            data_return('参数有误');
+        }
+        $data = $this->model->fetchData(['id'=>$param['id']]);
+        if (empty($data)){
+            data_return('用户不存在',-1);
+        }
+        $res = $data->saveData($data['id'],['status'=>intval($param['status'])]);
+        data_return('操作成功',0,$res);
+    }
+
+    public function get_data(Request $request)
+    {
+        $id = $request->get('id',0,'intval');
+        $data = $this->model->fetchData($id);
+
+        data_return('success',0,$data);
+    }
+}
